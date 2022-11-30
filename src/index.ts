a429a3917aa0fa514bdaf28266b8b55ae41e8386
95decode <95decode@gmail.com>
2022-11-30 18:38:07 +0900

    Add sharp example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("foo");
+import sharp from "sharp";
+
+async function composite() {
+    try {
+        await sharp("back.png")
+        .composite([{
+            input: "front.png",
+            top: 50,
+            left: 50
+        }])
+        .toFile("resulte.png")
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+composite();
