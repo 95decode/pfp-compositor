e56345b9e2fb0925070fa18ab39726521c813b77
95decode <95decode@gmail.com>
2022-12-01 21:01:39 +0900

    Add input as png

--- src/compositor/compositor.ts
+++ src/compositor/compositor.ts
@@ -2,11 +2,11 @@ import sharp from "sharp";
 
 export async function composite(back: string, front: string, res: string) {
     try {
-        await sharp(back)
+        await sharp(back + ".png")
         .composite([{
-            input: front
+            input: (front + ".png")
         }])
-        .toFile(res)
+        .toFile(res + ".png")
     } catch (err) {
         console.log(err);
     }
